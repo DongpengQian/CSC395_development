c0fa8a249511f568266760e2afb2ca34689a8b30
Dongpeng Qian <dongpeng.qian@stmartin.edu>
2024-10-29 16:23:17 -0700

    [commit message]

---
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreTodo.Services;
+using AspNetCoreTodo.Models;
+
+namespace AspNetCoreTodo.Controllers;
+
+public class TodoController : Controller
+{
+    private readonly ITodoItemService _todoItemService;
+
+    public TodoController(ITodoItemService todoItemService)
+    {
+        _todoItemService = todoItemService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+    var items = await _todoItemService.GetIncompleteItemsAsync();
+
+    var model = new TodoViewModel()
+    {
+        Items = items
+    };
+
+    return View(model);
+    }
+}   
