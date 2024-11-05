ccc0f1d7cc96c3a46d86b08660b81bf597e61a03
Dongpeng Qian <dongpeng.qian@stmartin.edu>
2024-11-05 14:10:47 -0800

    [commit message]

--- AspNetCoreTodo/Data/ApplicationDbContext.cs
+++ AspNetCoreTodo/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using AspNetCoreTodo.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace AspNetCoreTodo.Data;
@@ -15,4 +16,7 @@ public class ApplicationDbContext : IdentityDbContext
         base.OnModelCreating(builder);
         // ...
     }
+
+    public DbSet<TodoItem> Items { get; set; }
+    
 }
